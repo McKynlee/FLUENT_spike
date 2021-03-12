aadb9b5a07f10b1c72e325f5f52f274fbd174ecb
Johnshandsable <Jshands@asu.edu>
2021-03-12 09:49:43 -0600

    added and tested router - count

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -70,7 +70,20 @@ router.put('/:id', (req, res) => {
  * they have added to the shelf
  */
 router.get('/count', (req, res) => {
-  // endpoint functionality
+  const userId = req.user.id;
+  const sqlQuery = `SELECT "item".id, "description", "image_url" FROM "item"
+  JOIN "user" ON "item".user_id = "user".id 
+  WHERE "user".id = $1;`;
+
+  pool
+    .query(sqlQuery, [userId])
+    .then((dbRes) => {
+      console.log('GET /count successful');
+      res.send(dbRes.rows);
+    })
+    .catch((err) => {
+      console.error('GET /count an error occurred', err);
+    });
 });
 
 /**
